5c5c7a08c670c09eb4fb6611cb0970edcdb493ac
ivanmvh <ivanmvh@gmail.com>
2015-07-20 03:53:49 -0430

    Despliegue DB en Heroku - 2

--- models/models.js
+++ models/models.js
@@ -47,7 +47,7 @@ sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
   console.log("____46");
   Quiz.count().then(function (count){
-    if(count > 0) {   // la tabla se inicializa solo si está vacía
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.bulkCreate( 
         [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
           {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'},
