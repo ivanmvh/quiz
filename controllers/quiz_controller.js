9d5ef43b28fb9ed3c92d2256b9f0b9c366c88806
ivanmvh <ivanmvh@gmail.com>
2015-07-25 04:04:22 -0430

    P2P - MODULO 7 - BRANCH BUSQUEDAS PREGUNTAS -
    Se incorporo la Busqueda de Preguntas en un Form Get , Se uso iLike en
    vez de Like para hacer la busqueda insensitiva a Mayusculas o
    Minusculas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,11 +16,30 @@ exports.load = function(req,res,next,quizId)
 
 // GET /quizes
 exports.index = function(req, res) 
-  {console.log("quiz_controller index 5");
-   models.Quiz.findAll().then
-      (function(quizes) 
+  { console.log("--->>>  quiz_controller index 5");
+    //console.log("--->>>  req.query.search.length ="+ req.query.search.length);
+    console.log("--->>>  req.query.search ="+ req.query.search);
+    // var wtemp=req.query.search.trim();
+
+    var wtemp=req.query.search || ' ';
+    console.log("--->>> wtemp===null --->>> "+ wtemp===null) ;
+    var wtrim = (" " + wtemp).trim();
+    var wlen = wtrim.length;
+
+    if (wlen  > 0 )
+      { console.log("--->>>  si hay req.query.search ="+ wtrim);
+        models.Quiz.findAll({where: ["pregunta  ilike ?", ('%'+wtrim+'%').replace(/\s+/g,'%')]}).then
+        (function(quizes) 
+          {res.render('quizes/index.ejs', { quizes: quizes});}
+        ).catch(function(error) {next(error);})
+      }
+    else
+      { console.log("--->>>  no hay req.query.search ="+ req.query.search);
+        models.Quiz.findAll().then
+        (function(quizes) 
         {res.render('quizes/index.ejs', { quizes: quizes});}
-      ).catch(function(error) {next(error);})
+        ).catch(function(error) {next(error);})
+      };    
   };
 
 // GET /quizes/:id
@@ -29,10 +48,14 @@ exports.show = function(req, res)
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) 
-  { console.log("req.query.respuesta = req.quiz.respuesta" + req.query.respuesta + "="+req.quiz.respuesta);
+  { console.log( "--->>> req.query.respuesta = req.quiz.respuesta" + 
+                 req.query.respuesta.toUpperCase() + "=" + req.quiz.respuesta.toUpperCase());
+    
     var resultado = 'Incorrecta';
-    if  (req.query.respuesta === req.quiz.respuesta) 
+    
+    if  ((req.query.respuesta).toUpperCase().trim() === (req.quiz.respuesta).toUpperCase().trim()) 
         {resultado= 'Correcto'; }
+    
     res.render('quizes/answer', { quiz : req.quiz ,respuesta: resultado });
   };
 
