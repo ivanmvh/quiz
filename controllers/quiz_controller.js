cb0b9c5e92905a2ae872847f4003b9b832787226
ivanmvh <ivanmvh@gmail.com>
2015-07-31 06:22:37 -0430

    P2P MODULO 8 - INCORPORACION CAMPO TEMA EN QUIZES
    Tarea con problemita por uso de placeholder en vez de value. Con
    placeholder no toma los valores que no se tocan en edicion, dice que no
    estan llenos.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -65,7 +65,7 @@ exports.answer = function(req, res)
 exports.new = function(req, res) 
 {
   var quiz = models.Quiz.build
-  ({pregunta: " ", respuesta: " "});
+  ({pregunta: "P r e g u n t a", respuesta: "R e s p u e s t a ", tema: "T e m a "});
 
   res.render('quizes/new', { quiz: quiz , errors: []});
 };
@@ -84,7 +84,7 @@ exports.create = function(req, res)
             res.render('quizes/new', {quiz: quiz, errors: err.errors});  
           } else {
               // guarda en BD los campos pregunta y respuesta de quiz
-              quiz.save({fields: ["pregunta", "respuesta"]}).then(function()
+              quiz.save({fields: ["pregunta", "respuesta","tema"]}).then(function()
               {res.redirect('/quizes')})
           }
         }   
@@ -103,6 +103,7 @@ exports.update = function(req, res)
   {
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
     req.quiz.validate()
       .then (
         function(err){
@@ -110,7 +111,7 @@ exports.update = function(req, res)
             res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});  
           } else {
               // guarda en BD los campos pregunta y respuesta de quiz
-              req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function()
+              req.quiz.save({fields: ["pregunta", "respuesta","tema"]}).then(function()
               {res.redirect('/quizes')})
           }
         }   
--- models/quiz.js
+++ models/quiz.js
@@ -3,7 +3,8 @@ module.exports = function(sequelize, DataTypes)
 {  	return sequelize.define
 	('Quiz',
 		{pregunta:  {type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta la Pregunta"}}},
-	     respuesta: {type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta la Respuesta"}}}
+	     respuesta: {type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta la Respuesta"}}},
+	     tema: {type: DataTypes.STRING, allowNull: false, defaultValue: "otr"}
 		}
 	);
 }
